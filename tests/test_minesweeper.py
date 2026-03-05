0fc607467411c016046d2bad555b833ede3c4f1b
boukay37 <boukay372@gmail.com>
2026-03-05 16:02:59 +0100

    linting precommit

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,20 +1,24 @@
 #  tests/test_minesweeper.py
 import pytest
+
 from src import minesweeper
+
 
 def test_module_exists():
     assert minesweeper
+
 
 def test_place_mines():
     game = minesweeper.Minesweeper(3, 3, 2)
     game.place_mines()
     assert len(game.mines) == 2, "Il devrait y avoir 2 mines dans la grille"
 
+
 def test_reveal():
     import random
+
     random.seed(0)
     game = minesweeper.Minesweeper(3, 3, 2)
     game.place_mines()
     game.reveal(2, 2)
     assert game.revealed == {(2, 2)}
-
